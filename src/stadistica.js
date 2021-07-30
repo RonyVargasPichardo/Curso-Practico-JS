68b21c051e306b775a6f186f2aeaa02b6ae0a5e6
Rony Vargas <vargaspihardo@gmail.com>
2021-07-29 22:38:35 -0400

    cree una calculadora de la media geometrica de una lista de elementos

--- src/stadistica.js
+++ src/stadistica.js
@@ -76,7 +76,7 @@ function calcularModa (lista) {
     listCount = Object.entries(obj);           // combierto mi obj en un array para usar metodos del array.
     listCount.sort( (a, b ) => a[1] - b[1] )  //ordena mi array
 
-    let variasModas = listCount.filter( (ele) => listCount[listCount.length -1][1] === ele[1]); //los numeros que se repiten mas con la misma cantidad
+    let variasModas = listCount.filter( (ele) => listCount[listCount.length -1][1] === ele[1]); //los numeros que se repiten mas de la lista con la misma cantidad
     variasModas.map( (elemento) => elemento.pop() )   //me elimina el ultimo elemento 
     
     if(variasModas.length > 1) {
@@ -91,3 +91,26 @@ La MODA es: ${listCount[listCount.length -1][0]}.`)
 }
 calcularModa([2,2,2,2,4,6,6,6,6,5,7,8,9,10,10,10,10,23,65])
 calcularModa([1,1,1,1,5,5,5,4])
+
+
+
+
+/**RETO:    encontrar la media geometrica */
+
+//formula  = GM = (x1 * x2 * x3)^1/n 
+// donde x es cada uno de los elementos que conforma la lista.
+// y n es la cantidad de elementos que conforma la lista.
+
+function calcularMediaGeometrica(lista){
+    //ordeno mi lista
+    lista.sort( (a,b) => a - b);
+    //obtengo la cantidad de elementos de la lista
+    const cantidadElementos = lista.length;
+    //multiplico cada elemento de mi lista y obtengo el resultado
+    const resultMultiplicacion = lista.reduce( (acumulado, elemento) => acumulado * elemento);
+    //calculo el resultado de la multiplicacion elevandolo a la potencia Y obtengo la media geometrica(GM)
+    const GM = Math.pow(resultMultiplicacion, 1/cantidadElementos).toFixed(3);
+    console.log(`La Media Geometrica de este grupo de elementos: '${lista}'.
+ES: ${GM}.`) 
+}
+calcularMediaGeometrica([12,3,5,6,6,7,10,])
